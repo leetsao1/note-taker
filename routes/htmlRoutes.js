e6cf2cb35d04b80214d2a6e53863ce430a8e1604
Lee Tsao <leetsao1@gmail.com>
2020-09-19 00:22:37 -0700

    fixing server comment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,9 +10,9 @@ module.exports = function (app){
         res.sendFile(path.join(__dirname , "../public/notes.html"));
     });  
 
-    // app.get("*", function(req,res){
-    //     res.sendFile(path.join(__dirname, "../public/index.html"));
-    // });  
+    app.get("*", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });  
 
     
 };
